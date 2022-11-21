b82a727b37bf5c208f0639fd51e77fadb0f45965
Jevon Levin <jevon.levin@gmail.com>
2022-11-21 16:50:50 +0700

    fix: code reformat

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -3,42 +3,22 @@
 @section('content')
     <div class="px-5 py-3 d-flex flex-row justify-content-around">
         <div>
-            <h4>
-                Store address :
-            </h4>
-            <div>
-                Jalan Pembangunan Baru Raya,
-            </div>
-            <div>
-                Kompleks Pertokoan Emerald Blok III/12
-            </div>
-            <div>
-                Bintaro, Tangerang Selatan
-            </div>
-            <div>
-                Indonesia
-            </div>
+            <h4>Store address :</h4>
+            <div>Jalan Pembangunan Baru Raya,</div>
+            <div>Kompleks Pertokoan Emerald Blok III/12</div>
+            <div>Bintaro, Tangerang Selatan</div>
+            <div>Indonesia</div>
         </div>
 
         <div>
-            <h4>
-                Open Daily:
-            </h4>
-            <div>
-                08.00-20.00
-            </div>
+            <h4>Open Daily:</h4>
+            <div>08.00-20.00</div>
         </div>
 
         <div>
-            <h4>
-                Contact:
-            </h4>
-            <div>
-                Phone : 021-08899776655
-            </div>
-            <div>
-                Email : happybookstore@happy.com
-            </div>
+            <h4>Contact:</h4>
+            <div>Phone : 021-08899776655</div>
+            <div>Email : happybookstore@happy.com</div>
         </div>
     </div>
 @endsection
